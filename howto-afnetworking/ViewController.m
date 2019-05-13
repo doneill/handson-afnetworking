dbec30250d55c5b34f8b620594751a3e5d4788cb
Jose Silva <jsilva@codavel.com>
2019-05-13 18:06:54 +0100

    source code

--- howto-afnetworking/ViewController.m
+++ howto-afnetworking/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
 
 @interface ViewController ()
 
@@ -16,8 +17,57 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self performGETRequest];
+    [self performPOSTRequest];
 }
 
+- (void) performGETRequest{
+    // 1 - define resource URL
+    NSURL *URL                      = [NSURL URLWithString:@"https://httpbin.org/get"];
+    
+    //2 - create AFNetwork manager
+    AFHTTPSessionManager *manager   = [AFHTTPSessionManager manager];
+    
+    //3 - set a cache policy
+    [manager.requestSerializer setCachePolicy:NSURLRequestReloadIgnoringLocalCacheData];
+    
+    //4 - create request
+    [manager    GET:URL.absoluteString
+         parameters:nil
+           progress:nil
+     //5 - response handling
+            success:^(NSURLSessionTask *task, id responseObject) {
+                NSLog(@"Reply GET JSON: %@", responseObject);
+            }
+            failure:^(NSURLSessionTask *operation, NSError *error) {
+                NSLog(@"Error: %@", error);
+            }
+     ];
+}
 
+- (void) performPOSTRequest{
+    // 1 - define resource URL
+    NSURL *URL                      = [NSURL URLWithString:@"https://httpbin.org/post"];
+    
+    //2 - create AFNetwork manager
+    AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc]initWithSessionConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    [manager.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    //3 - set a body
+    NSDictionary *body = @{@"Getting": @{@"Started":@{@"With":@{@"Bolina":@"go to"}},@"url":@"https://www.codavel.com/trybolina/"}};
+    //4 - create request
+    [manager POST:URL.absoluteString
+       parameters:body
+         progress:nil
+     //5 - response handling
+          success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+              NSLog(@"Reply POST JSON: %@", responseObject);
+          }
+          failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+              NSLog(@"error: %@", error);
+          }
+     ];
+}
 @end
